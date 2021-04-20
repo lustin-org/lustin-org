aab402e45cb6fc924b4f6a2e1e90da266e5c0a3e
Al Iczin <al@iczin.guru>
2021-04-20 09:15:09 +0300

    второй вложенный репозиторий

--- local-prod-deploy.sh
+++ local-prod-deploy.sh
@@ -1,6 +1,10 @@
 git pull origin main
 (cd hbd; git pull origin main)
+(cd iclude/lazyeco; git pull origin main)
+(cd iclude/pggy; git pull origin main)
 git add hbd
+git add iclude/lazyeco
+git add iclude/pggy
 git commit -m "Локальное развертывание актуальные версии контента"
 git push origin main
 npm run deploy
